2008991d3b934a69880a077acd4e74ba484ffc5e
dwightlai <laidehua771104@gmail.com>
2026-05-28 11:39:34 +0800

    fix: make foundation backend locally runnable

---
+++ server/src/test/java/com/aiworkflow/workflow/api/WorkflowSecurityIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.aiworkflow.workflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(
+        properties = {
+                "spring.autoconfigure.exclude="
+                        + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+                        + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+        })
+@AutoConfigureMockMvc
+class WorkflowSecurityIntegrationTest {
+    private final MockMvc mockMvc;
+
+    @Autowired
+    WorkflowSecurityIntegrationTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void listWorkflowsReturnsOkWithSecurityFiltersEnabled() throws Exception {
+        mockMvc.perform(get("/api/workflows"))
+                .andExpect(status().isOk());
+    }
+}
